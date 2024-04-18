9bb4749233f198e2ba2eb5d7d81b9c2844ead9ff
DevAlchamist <nayakshubhanshu060705@gmail.com>
2024-04-18 11:11:20 +0530

    New Branch

---
+++ src/Components/HomeComp/PaymentBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import amazon from "@/Assets/Homepage/PaymentBar/amazon.png";
+import bhim from "@/Assets/Homepage/PaymentBar/bhim-upi-100.png";
+import bajaj from "@/Assets/Homepage/PaymentBar/bajaj-finserv.png";
+import Paytm from "@/Assets/Homepage/PaymentBar/Paytm.png";
+import Gpay from "@/Assets/Homepage/PaymentBar/Gpay.png";
+import Razor from "@/Assets/Homepage/PaymentBar/razorpay 1.png";
+import upi from "@/Assets/Homepage/PaymentBar/upi-ar21 1.png";
+import rupay from "@/Assets/Homepage/PaymentBar/rupay.png";
+
+import Image from "next/image";
+
+const PaymentBar = () => {
+  return (
+    <div className="bg-[#000000] h-[136px] flex items-center justify-around">
+      <Image src={amazon} alt="amazon" />
+      <Image src={bhim} alt="Bhim" />
+      <Image src={bajaj} alt="Bajaj" />
+      <Image src={Paytm} alt="Paytm" />
+      <Image src={Gpay} alt="Gpay" />
+      <Image src={Razor} alt="Razor" />
+      <Image src={upi} alt="Upi" />
+      <Image src={rupay} alt="Rupay" />
+    </div>
+  );
+};
+
+export default PaymentBar;
