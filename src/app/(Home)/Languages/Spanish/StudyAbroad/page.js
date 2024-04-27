8eefbafa06cd22abffc5f5059ee87d05d543bb38
arafatrahat94 <arafatrahat94@gmail.com>
2024-04-27 14:02:37 +0600

    fixed component issue

--- src/app/(Home)/Languages/Spanish/StudyAbroad/page.js
+++ src/app/(Home)/Languages/Spanish/StudyAbroad/page.js
@@ -12,8 +12,7 @@ import PaymentBar from "@/Components/HomeComp/PaymentBar";
 
 import FaqsSudyAbroad from "./Components/FaqsSudyAbroad";
 import StudyAbroad from "./Components/StudyAbroad";
-
-import EverythingYouNeedToKnowAboutGerman from "../Components/EverythingYouNeedToKnowAboutSpanish/EverythingYouNeedToKnowAboutSpanish.jsx";
+import EverythingYouNeedToKnowAboutSpanish from "../Components/EverythingYouNeedToKnowAboutSpanish/EverythingYouNeedToKnowAboutSpanish";
 
 const FrenchForStudyAbroadPage = () => {
   return (
@@ -27,7 +26,7 @@ const FrenchForStudyAbroadPage = () => {
       <ClassesForAnyLevel />
       <StudentsTestomony />
       <FaqsSudyAbroad />
-      <EverythingYouNeedToKnowAboutGerman />
+      <EverythingYouNeedToKnowAboutSpanish />
       <div>
         <div className="w-full bg-primary-color max-sm:h-[200px] h-[380px] relative">
           <div className=" h-full flex items-center text-center text-white text-5xl max-sm:text-2xl max-lg:text-4xl font-bold justify-center">
