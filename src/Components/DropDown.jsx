5f081b417cf477fd816be8b97e67efc3c49e7b51
DevAlchamist <nayakshubhanshu060705@gmail.com>
2024-04-29 23:12:04 +0530

    DropDown-Fixes-Done-1

---
+++ src/Components/DropDown.jsx
@@ -0,0 +1,64 @@
+"use client";
+import { data } from "autoprefixer";
+import Link from "next/link";
+import { useState } from "react";
+
+const Dropdown = (NavData) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div
+        className="relative group"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <button className="inline-flex outline-none items-center justify-center w-full px-4 py-2 text-[20px]  bg-white rounded-md shadow-sm  ">
+          <span className="mr-2">{NavData.data.title}</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-5 h-5 ml-2 -mr-1"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fillRule="evenodd"
+              d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <div
+          className={`absolute right-0 z-50 w-full ${
+            NavData.data.title === "Languages" ? "h-[180px]" : "h-fit"
+          } overflow-y-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1 ${
+            isHovered ? "" : "hidden"
+          }`}
+          onMouseEnter={handleMouseEnter} // Keep dropdown open when hovering over dropdown items
+        >
+          {/* Dropdown content goes here */}
+          {NavData.data.data.map((Nav) => (
+            <Link
+              key={Nav.key}
+              href={Nav.path ? Nav.path : "#"}
+              className="block px-4 py-2  hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md"
+            >
+              {Nav.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
