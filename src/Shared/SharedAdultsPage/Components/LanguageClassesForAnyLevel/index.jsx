58d2f2e2f814b7ccdc59e4cb53a7d0abc541efc6
arafatrahat94 <arafatrahat94@gmail.com>
2024-05-13 20:53:31 +0600

    responsiveness of adult page is done fully

---
+++ src/Shared/SharedAdultsPage/Components/LanguageClassesForAnyLevel/index.jsx
@@ -0,0 +1,25 @@
+import ClassLevelCard from "./ClassLevelCard";
+
+const LanguageClassesForAnyLevel = ({ ClassesForAnyLevel }) => {
+  // console.log(ClassesForAnyLevel);
+  return (
+    <div className=" my-[112px] w-full mx-auto">
+      <div className="text-black max-md:text-2xl lg:text-4xl max-lg:text-3xl text-[60px] mb-12 text-center font-bold ">
+        {ClassesForAnyLevel?.Language} Classes for Any Level
+      </div>
+      <div
+        className={`${
+          ClassesForAnyLevel?.CardData?.length === 3 && "xl:grid-cols-3"
+        } ${ClassesForAnyLevel?.CardData?.length === 4 && "xl:grid-cols-4"} ${
+          ClassesForAnyLevel?.CardData?.length === 1 ? "grid" : ""
+        } xl:grid 2xl:max-w-[1640px] max-w-8xl justify-center gap-1 max-md:gap-5 md:gap-2 px-5 mx-auto max-sm:px-4 w-full lg:gap-[18px] md:grid-cols-2  items-center mt-[48px] `}
+      >
+        {ClassesForAnyLevel?.CardData?.map((data, index) => (
+          <ClassLevelCard key={index} data={data} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LanguageClassesForAnyLevel;
--- src/app/(Home)/Languages/English/adults/page.js
+++ src/app/(Home)/Languages/English/adults/page.js
@@ -22,248 +22,197 @@ import Faqs from "@/Shared/FrenchAdultReusableComponents/Faqs/Faqs";
 import LanguageLessons from "@/Shared/FrenchAdultReusableComponents/Languagelessons/LanguageLessons";
 import { list } from "postcss";
 import HowItWorks from "@/Shared/FrenchAdultReusableComponents/HowItWorks/HowItWorks";
+import SharedAdultsPage from "@/Shared/SharedAdultsPage";
 
 const page = () => {
-  const bannerData = {
-    title: "Online English Classes for Adults",
-    para: "Learning a new language has no age limit! If you're ready to dive into the world of English, you're in luck! Our online English classes and courses for adults offer an engaging and hassle-free way to learn one of the most widely spoken languages globally. Whether you're just starting out or already have a foundation, our expert instructors will be your guides through the maze of English grammar, vocabulary, and culture. With classes tailored to your schedule, you'll be speaking English from day one! Our top-notch English trainers are here to provide personalised support and lead you through an enriching learning experience, through 1:1 or group Online English classes for adults.",
-    images: [img1, img2, img3],
+  const data = {
+    BannerData: {
+      Language: "English",
+      Description:
+        "Learning a new language has no age limit! If you're ready to dive into the world of English, you're in luck! Our online English classes and courses for adults offer an engaging and hassle-free way to learn one of the most popular languages in the world. Whether you're just starting out or already have a foundation, our expert instructors will be your guides through the maze of English grammar, vocabulary, and culture. With classes tailored to your schedule, you'll be speaking English from day one! Our top-notch English trainers are here to provide personalised support and lead you through an enriching learning experience, through 1:1 or group Online English classes for adults.",
+    },
+    HowItWorksCardData: [
+      {
+        id: 1,
+        title: "Register with us",
+        description:
+          "Fill out our form with your details and your preferred language to learn with us.",
+        img: step1,
+      },
+      {
+        id: 2,
+        title: "Talk To Our Expert",
+        description:
+          "Connect with our Academic Counsellors for personalized guidance.",
+        img: step2,
+      },
+      {
+        id: 3,
+        title: "Get A Free Demo",
+        description:
+          "Experience our platform firsthand with a complimentary demo.",
+        img: step3,
+      },
+      {
+        id: 4,
+        title: "Get Started With Us",
+        description:
+          "Love our demo- now pick and choose batches according to your flexibility and get started on your learning adventure with us.",
+        img: step4,
+      },
+    ],
+    LanguageExams: {
+      Language: "English",
+      CardData: [
+        {
+          title: "IELTS preparation classes",
+          img: dalf,
+          description:
+            "Take your English skills to new heights with our IELTS preparation course. Designed to help you excel in all four components - reading, writing, listening, and speaking - our expert instructors will guide you towards success in this globally recognized English proficiency exam.",
+          link: "",
+        },
+        {
+          title: "TOEFL preparation classes",
+          img: telf,
+          description:
+            "Open doors to international opportunities with our TOEFL preparation course. From mastering academic English to excelling in listening, speaking, reading, and writing, our comprehensive training ensures you're fully prepared to ace this crucial exam.",
+          link: "",
+        },
+      ],
+    },
+    ClassesForAnyLevel: {
+      Language: "English",
+
+      CardData: [
+        {
+          title: "A1-A2",
+          level: "Beginner Classes",
+          description:
+            "Start your English language journey with our engaging online classes designed for beginners. Learn fundamental grammar, vocabulary, and practical conversation skills at your own pace with our expert instructors' guidance.",
+          link: "",
+        },
+        {
+          title: "B1-B2",
+          level: "Intermediate Classes",
+          description:
+            "Elevate your English proficiency with our online intermediate classes. Refine pronunciation, expand vocabulary, and master complex grammar structures to confidently engage in conversations and express yourself fluently.",
+          link: "",
+        },
+        {
+          title: "C1-C2",
+          level: "Advanced Classes",
+          description:
+            "Advance your English skills to the highest level with our online advanced classes. Enhance your conversational abilities, master complex grammar, and increase career prospects with fluent proficiency in English.",
+          link: "",
+        },
+      ],
+    },
+    Faqs: [
+      {
+        question: "Why learn with The Language Network?",
+        answer:
+          "Learn with us for personalised instruction, flexible scheduling, and immersive learning experiences tailored to your specific needs. Our expert tutors offer 1:1 and small group classes, ensuring personalised attention and effective learning. Additionally, we provide recordings of classes for your convenience and offer free study material worth ₹10,000+ to support your language learning journey.",
+      },
+
+      {
+        question: "Will I get a certificate after completing each level?",
+        answer:
+          "Yes, you will receive a certificate upon completion of each level. We, as a language school are ISO-certified and our certificates are PAN India recognised. We also prepare for language proficiency exams, the certificates for which are recognised worldwide. ",
+      },
+      {
+        question:
+          "What job opportunities will I get after completing my French course?",
+        answer:
+          "Upon completing your English course you’ll gain access to job opportunities in teaching, translation, content creation, communication, international business, tourism, education, media, technical writing, human resources, and more.",
+      },
+
+      {
+        question: "Is English an easy language to learn?",
+        answer:
+          "According to the Foreign Service Institute (FSI), English is classified as a Category I language, meaning it is relatively easy for speakers of other languages to learn. It has a straightforward alphabet, simple grammar rules , and a wealth of resources available for learners.",
+      },
+
+      {
+        question: "Is English useful in India?",
+        answer:
+          "Yes, English is highly useful in India. It is widely spoken and serves as an official language alongside Hindi in many states and territories. English proficiency is essential for communication in business, education, government, media, and various other sectors. It also provides access to a vast array of global resources, opportunities, and networks.",
+      },
+
+      {
+        question: "How much time will it take to learn English?",
+        answer:
+          "The time it takes to learn English varies depending on your specific goal, such as achieving proficiency in exams like IELTS or TOEFL. It requires dedicated study and practice to reach your desired level.",
+      },
+
+      {
+        question: "What if I miss a class?",
+        answer:
+          "Missing a class should not be a worry as you will have lifetime access to the recorded classes and can refer to them at your convenience.",
+      },
+
+      {
+        question: "Will I be provided with study material?",
+        answer:
+          "Yes, you'll be provided with comprehensive study material worth ₹10,000 + free of cost to supplement your learning and reinforce key concepts.",
+      },
+
+      {
+        question: "Who will be teaching me?",
+        answer:
+          "Your learning journey is guided by internationally certified trainers who are experienced and proficient in both French language and teaching methodologies.",
+      },
+
+      {
+        question: "How many students are accommodated in one batch?",
+        answer:
+          "Our batches are usually small in size, typically accommodating 5 students to ensure personalised attention.",
+      },
+    ],
+    SayBonjur: "Learn from the finest!",
+    LanguageLesson: [
+      {
+        title: "Popular classes we offer        ",
+        list: [
+          "English Classes In Mumbai",
+          " English language course in delhi",
+          " English classes in pune",
+          "English language courses near me",
+          "English language classes near me",
+        ],
+      },
+      {
+        title: "Most Searched",
+        list: [
+          "English Speaking Course",
+          "English Language Classes Online",
+          "English Speaking classes         ",
+          "English Speaking Course online          ",
+        ],
+      },
+      {
+        title: "Popular classes by target group ",
+        list: [
+          "Online English Course          ",
+          "English classes online          ",
+          "English Speaking Course",
+        ],
+      },
+      {
+        title: "English Classes for Focused Learning",
+        list: [
+          "Online English classes for Beginner",
+          "Online English classes for Intermediate",
+          "Online English classes for Advanced",
+          "Online English classes for Kids ",
+          "Online English classes for adults",
+          "Online English classes to study abroad",
+        ],
+      },
+    ],
   };
-  const levelData = [
-    {
-      title: "A1-A2",
-      title2: "Beginner Classes",
-      description:
-        "Start your English language journey with our engaging online classes designed for beginners. Learn fundamental grammar, vocabulary, and practical conversation skills at your own pace with our expert instructors' guidance.",
-      link: "",
-    },
-    {
-      title: "B1-B2",
-      title2: "Intermediate Classes",
-      description:
-        "Elevate your English proficiency with our online intermediate classes. Refine pronunciation, expand vocabulary, and master complex grammar structures to confidently engage in conversations and express yourself fluently.",
-      link: "",
-    },
-    {
-      title: "C1-C2",
-      title2: "Advanced Classes",
-      description:
-        "Advance your English skills to the highest level with our online advanced classes. Enhance your conversational abilities, master complex grammar, and increase career prospects with fluent proficiency in French.",
-      link: "",
-    },
-  ];
-  const LearnData = `Learn from the finest!`;
-  const PrepareData = [
-    {
-      title: "IELTS preparation classes",
-      img: dalf,
-      description:
-        "Take your English skills to new heights with our IELTS preparation course. Designed to help you excel in all four components - reading, writing, listening, and speaking - our expert instructors will guide you towards success in this globally recognized English proficiency exam.",
-      link: "",
-    },
-    {
-      title: "TOEFL preparation classes",
-      img: telf,
-      description:
-        "Open doors to international opportunities with our TOEFL preparation course. From mastering academic English to excelling in listening, speaking, reading, and writing, our comprehensive training ensures you're fully prepared to ace this crucial exam.",
-      link: "",
-    },
-  ];
-  const TestimonialData = [
-    {
-      index: 1,
-      para: "“French I have done my French A1 & A2 from The Language Network. I have an amazing experience with the teachers. They teach from the deep down of their soul. Anytime they are ready to help. Specially Amrita Iyer is very very helpful to learn French. Thank you The Language Network!!!“",
-      img: "",
-      name: "Komal Patil",
-      Dept: "MBA in Marketing",
-      Rating: "2",
-    },
-    {
-      index: 2,
-      para: "“French whenever it comes to language learning, TLN is the best place I must say. Earlier I was too skeptical to join online classes. But TLN  makes it a cakewalk   for you to learn  a foreign language. I'm looking forward for enthusiastic and communicative classes as it was before.”",
-      img: "",
-      name: "Pranjal Shinde",
-      Dept: "Post Graduate Sudent",
-      Rating: "3",
-    },
-    {
-      index: 3,
-      para: "“Up to now, my experience with The Language Network has been exceptional. Prajakta, my French instructor, created a friendly and comfortable atmosphere in the classes, which made the learning process enjoyable and engaging for me.”",
-      img: "",
-      name: "Rithika Kumari",
-      Dept: "Student",
-      Rating: "4.5",
-    },
-    {
-      index: 4,
-      para: "“The classes are conducted on time. And its basically 3 or 4 students per batch, so everything is understood  and doubts are solved instantly. I'm very happy that I found out about The Language Network, its really great.”",
-      img: "",
-      name: "Arti Darekar",
-      Dept: "Student",
-      Rating: "3",
-    },
-    {
-      index: 5,
-      para: "“Initially, I was very nervous about how to go about learning a new language but Samihan made it extremely comfortable for all of us. He used unique and fun techniques to make sure we understand. I am so glad to have chosen TLN for this wonderful experience.”",
-      img: "",
-      name: "Komal Patil",
-      Dept: "MBA in Marketing",
-      Rating: "5",
-    },
-  ];
-
-  const Faq = [
-    {
-      question: "Why learn with The Language Network?",
-      answer:
-        "Learn with us for personalised instruction, flexible scheduling, and immersive learning experiences tailored to your specific needs. Our expert tutors offer 1:1 and small group classes, ensuring personalised attention and effective learning. Additionally, we provide recordings of classes for your convenience and offer free study material worth ₹10,000+ to support your language learning journey.",
-    },
-
-    {
-      question: "Will I get a certificate after completing each level?",
-      answer:
-        "Yes, you will receive a certificate upon completion of each level. We, as a language school are ISO-certified and our certificates are PAN India recognised. We also prepare for language proficiency exams, the certificates for which are recognised worldwide. ",
-    },
-    {
-      question:
-        "What job opportunities will I get after completing my French course?",
-      answer:
-        "Upon completing your English course you’ll gain access to job opportunities in teaching, translation, content creation, communication, international business, tourism, education, media, technical writing, human resources, and more.",
-    },
-
-    {
-      question: "Is English an easy language to learn?",
-      answer:
-        "According to the Foreign Service Institute (FSI), English is classified as a Category I language, meaning it is relatively easy for speakers of other languages to learn. It has a straightforward alphabet, simple grammar rules , and a wealth of resources available for learners.",
-    },
-
-    {
-      question: "Is English useful in India?",
-      answer:
-        "Yes, English is highly useful in India. It is widely spoken and serves as an official language alongside Hindi in many states and territories. English proficiency is essential for communication in business, education, government, media, and various other sectors. It also provides access to a vast array of global resources, opportunities, and networks.",
-    },
-
-    {
-      question: "How much time will it take to learn English?",
-      answer:
-        "The time it takes to learn English varies depending on your specific goal, such as achieving proficiency in exams like IELTS or TOEFL. It requires dedicated study and practice to reach your desired level.",
-    },
-
-    {
-      question: "What if I miss a class?",
-      answer:
-        "Missing a class should not be a worry as you will have lifetime access to the recorded classes and can refer to them at your convenience.",
-    },
-
-    {
-      question: "Will I be provided with study material?",
-      answer:
-        "Yes, you'll be provided with comprehensive study material worth ₹10,000 + free of cost to supplement your learning and reinforce key concepts.",
-    },
-
-    {
-      question: "Who will be teaching me?",
-      answer:
-        "Your learning journey is guided by internationally certified trainers who are experienced and proficient in both French language and teaching methodologies.",
-    },
-
-    {
-      question: "How many students are accommodated in one batch?",
-      answer:
-        "Our batches are usually small in size, typically accommodating 5 students to ensure personalised attention.",
-    },
-  ];
-  const lessonData = [
-    {
-      title: "Popular classes We Offer",
-      list: [
-        "Online French Classes",
-        "Online Spanish Classes",
-        "Online German Classes",
-        "Online Mandarin Classes",
-        "Online Japanese Classes ",
-        "Online Korean Classes",
-        "Online English Classes",
-      ],
-    },
-    {
-      title: "Popular Online classes",
-      list: [
-        "Online French Grammar Classes",
-        "Online French Speaking Classes",
-        "Online English Grammar Classes",
-        "Online English Speaking Classes",
-        "Online Spanish Grammar Classes",
-        "Online Spanish Speaking Classes",
-      ],
-    },
-    {
-      title: "Popular classes by target group",
-      list: [
-        "Online French Classes for kids",
-        "Online French Classes for adults",
-        "Online French Classes for corporates",
-        "Online English Classes for kids",
-        "Online English Classes for adults",
-        "Online English Classes for corporates",
-      ],
-    },
-    {
-      title: "Popular online classes by level",
-      list: [
-        "Online French Classes for Beginners",
-        "Online French Classes for Intermediate",
-        "Online French Classes for Advance",
-        "Online English Classes for Beginners",
-        "Online English Classes for Intermediate",
-        "Online English Classes for Advance",
-      ],
-    },
-  ];
-  const howData = [
-    {
-      index: 1,
-      step: "Step 1",
-      title: "Register with us",
-      para: "Fill out our form with your details and your preferred language to learn with us.",
-      img: step1,
-    },
-    {
-      index: 2,
-      step: "Step 2",
-      title: "Talk to our experts",
-      para: "Connect with our Academic Counsellors for personalized guidance.",
-      img: step2,
-    },
-    {
-      index: 3,
-      step: "Step 3",
-      title: "Get a free Demo",
-      para: "Experience our platform firsthand with a complimentary demo.",
-      img: step3,
-    },
-    {
-      index: 4,
-      step: "Step 4",
-      title: "Get Started With us",
-      para: "Love our demo- now pick and choose batches according to your flexibility and get started on your learning adventure with us.",
-      img: step4,
-    },
-  ];
   return (
     <div className="">
-      <PageMainBanner bannerData={bannerData} />
-      <HowItWorks data={howData} />
-      <WhyShould />
-      <UpcomingBatches />
-      <ClassesForAnyLevel levelData={levelData} />
-      <PreapareForExam data={PrepareData} />
-      <StudentsTestomony data={TestimonialData} />
-      <Faqs data={Faq} />
-      <Learn data={LearnData} />
-      <LanguageLessons data={lessonData} />
-      <SubscribeToOur />
-      <PaymentBar />
+      <SharedAdultsPage Data={data} />
     </div>
   );
 };
