7b11c3dd6db4fd5d5c636e2cf12f79de6fcb1dbd
hemant <hemvish11@gmail.com>
2024-07-08 21:54:37 +0530

    upcoming batches of homepage made responsive

--- src/Components/HomeComp/FluencyCultureCard.jsx
+++ src/Components/HomeComp/FluencyCultureCard.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const FluencyCultureCard = ({ datas }) => {
   return (
-    <div className="flex flex-col p-6 shadow-md hover:shadow-xl transition duration-200 text-center bg-white rounded-2xl min-h-full lg:min-h-[548px] mx-2 md:mx-10 xl:mx-0  lg:mx-[50px] w-full xl:max-w-[400px] 2xl:max-w-[420px] 3xl:max-w-[450px] 4xl:w-[548px] max-md:px-5">
+    <div className="flex flex-col p-6 shadow-md hover:shadow-xl transition duration-200 text-center bg-white rounded-2xl min-h-full lg:min-h-[548px] mx-2 md:mx-10 xl:mx-0 lg:mx-[50px] w-full max-w-[600px] xl:max-w-[400px] 2xl:max-w-[420px] 3xl:max-w-[450px] 4xl:w-[548px] max-md:px-5">
       <Image alt="step1" src={datas?.img} />
       <div className="flex flex-col pb-9 mt-4 shadow-md rounded-lg border border-teal-600 flex-grow lg:min-h-[298px] xl:min-h-[288px] border-solid max-md:max-w-full">
         <div className="justify-center py-4 text-3xl bg-teal-600 rounded-t-lg text-neutral-50 max-md:max-w-full">
