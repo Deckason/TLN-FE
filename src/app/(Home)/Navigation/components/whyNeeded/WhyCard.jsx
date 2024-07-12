81ca811ac1fece1104e67789d9c8242b0af38f82
Hemant Vishwakarma <hemvish11@gmail.com>
2024-07-12 23:23:53 +0530

    banner for college page made

---
+++ src/app/(Home)/Navigation/components/whyNeeded/WhyCard.jsx
@@ -0,0 +1,16 @@
+// import Image from "next/image";
+// import React from "react";
+
+// const WhyCard = ({ icon, title, description }) => {
+//   return (
+//     <div className="flex flex-col items-center justify-between">
+//       <div className="flex flex-col justify-center items-center">
+//         <Image src={icon} alt={`icon`} width={500} height={500} className="w-[50px]" />
+//         <p>{title}</p>
+//       </div>
+//       <p>{description}</p>
+//     </div>
+//   );
+// };
+
+// export default WhyCard;
---
+++ src/app/(Home)/Navigation/components/whyNeeded/WhyNeeded.jsx
@@ -0,0 +1,15 @@
+// import WhyCard from "./WhyCard";
+// const WhyNeeded = ({institute,cardInfo}) => {
+  
+//   return <div>
+//     <h2>Why {institute} need foreign languages?</h2>
+//     <div>
+//       {cardInfo.map((card)=>{
+//         return (
+//           <WhyCard id={card.title} icon={card.icon} title={card.title} description={card.description}/>
+//         )
+//       })}
+//     </div>
+//   </div>;
+// };
+// export default WhyNeeded;
