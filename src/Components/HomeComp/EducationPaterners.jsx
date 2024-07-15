f0967b30b67757a21daa013815de9217dcd4b882
Hemant Vishwakarma <hemvish11@gmail.com>
2024-07-15 19:51:56 +0530

    Our Educational Partners margin added

--- src/Components/HomeComp/EducationPaterners.jsx
+++ src/Components/HomeComp/EducationPaterners.jsx
@@ -3,7 +3,7 @@ import logo1 from "../../Assets/Homepage/Educationalbar/EducationalBar.png";
 const EducationPartners = () => {
   return (
     <div>
-      <div className="my-[112px] max-w-[1681px]  mx-auto flex justify-center items-center ">
+      <div className=" my-[50px] lg:my-[112px] max-w-[1681px]  mx-auto flex justify-center items-center ">
         <section>
           <div>
             <h1 className="text-stone-900 text-[30px]/[45px] sm:text-[40px]/[55px] md:text-[45px]/[60px] lg:text-[50px]/[65px] xl:text-[55px]/[70px] 2xl:text-[60px]/[72px] font-bold text-center mb-[20.9px]">
