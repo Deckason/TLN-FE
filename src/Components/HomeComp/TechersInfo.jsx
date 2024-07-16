68b6b989546e47df0af24b0162f313b63af2bfa6
Hemant Vishwakarma <hemvish11@gmail.com>
2024-07-16 15:41:40 +0530

    Corrected book a free demo button position in teacher info section

--- src/Components/HomeComp/TechersInfo.jsx
+++ src/Components/HomeComp/TechersInfo.jsx
@@ -6,10 +6,10 @@ import Image from "next/image";
 const TechersInfo = () => {
   return (
     <div className="lg:py-[112px] lg:flex block flex-col justify-center items-center p-20">
-      <div className="max-w-[1681px] p-5  justify-start items-center xl:gap-[72px] gap-[16px] lg:gap-[72px] flex-col 2xl:flex-row xl:flex-row lg:flex-row flex ">
-        <div className="lg:w-[490px] 2xl:w-[490px] w-[350px] xl:w-[490px] lg:h-[495px] flex-col justify-start items-start gap-10 inline-flex">
-          <div className="self-stretch lg:h-[389px] flex-col justify-start items-start gap-4 lg:gap-8 flex xl:z-30">
-            <div className="self-stretch lg:h-[190px] flex-col justify-start items-start gap-2 flex">
+      <div className="max-w-[1681px] p-5 justify-center items-center 2xl:gap-[72px] gap-[16px] lg:gap-[36px] flex-col 2xl:flex-row flex ">
+        <div className="w-full 2xl:w-[490px] 2xl:h-[495px] flex-col justify-center items-center gap-10 max-2xl:self-start  inline-flex">
+          <div className="self-start 2xl:h-[389px] flex-col justify-start items-start gap-4 2xl:gap-8 flex xl:z-30 w-full">
+            <div className="self-start lg:h-[190px] flex-col justify-start items-start gap-2 flex">
               <div className="text-stone-900 mb-6 text-2xl lg:text-[50px] font-bold ">
                 Inspiring.
               </div>
@@ -20,7 +20,7 @@ const TechersInfo = () => {
                 Knowledgeable.
               </div>
             </div>
-            <div className="self-stretch lg:h-[167px] text-stone-900 text-sm font-medium lg:text-lg lg:font-normal leading-7">
+            <div className="self-stretch 2xl:h-[167px] text-stone-900 text-sm font-medium lg:text-lg lg:font-normal leading-7">
               Meet our team of over 200+ internationally certified trainer who
               bring learning to life! With their expertise, they transform every
               lesson into a fun and engaging experience, creating a warm and
@@ -28,8 +28,8 @@ const TechersInfo = () => {
               <br />
             </div>
           </div>
-          <div className="self-stretch max-lg:hidden h-[58px] flex-col inset-x-0 bottom-0 justify-start items-start gap-2.5 flex"> 
-            <BookAFreeDemoButton /> 
+          <div className="self-stretch max-2xl:hidden h-[58px] flex-col inset-x-0 bottom-0 justify-start items-start gap-2.5 flex">
+            <BookAFreeDemoButton />
           </div>
         </div>
         <div className="w-full opacity-100 gap-x-4 flex-grow lg:h-[560px] h-full flex xl:z-20">
@@ -42,10 +42,10 @@ const TechersInfo = () => {
               className="w-full h-[185px] object-cover lg:h-full rounded lg:rounded-2xl"
             ></Image>
             <div className="w-full h-[50.62px]   absolute bottom-0 flex-col flex justify-center items-center bg-gradient-to-b from-black/10 to-black/10 rounded-bl rounded-br">
-              <div className="text-white text-5xl mt-[-6rem] font-bold ">Prachi</div>
-              <div className="text-white text-3xl mt-2  ">
-                French Trainer
+              <div className="text-white text-5xl mt-[-6rem] font-bold ">
+                Prachi
               </div>
+              <div className="text-white text-3xl mt-2  ">French Trainer</div>
             </div>
           </div>
           <div className="w-[25%]  h-full   rounded relative lg:rounded-2xl">
@@ -57,7 +57,9 @@ const TechersInfo = () => {
               className="w-full object-cover h-[185px] lg:h-full rounded lg:rounded-2xl"
             ></Image>
             <div className="w-full h-[50.62px]   absolute bottom-0 flex-col flex justify-center items-center bg-gradient-to-b from-black/10 to-black/10 rounded-bl rounded-br">
-              <div className="text-white text-4xl mt-[-4rem] font-bold ">Samihan</div>
+              <div className="text-white text-4xl mt-[-4rem] font-bold ">
+                Samihan
+              </div>
               {/* <div className="text-white text-[9.36px] font-normal ">
                 French Trainer
               </div> */}
@@ -72,9 +74,14 @@ const TechersInfo = () => {
               className="w-full object-cover h-[185px] lg:h-full rounded lg:rounded-2xl"
             ></Image>
             <div className="w-full h-[50.62px]   absolute bottom-0 flex-col flex justify-center items-center bg-gradient-to-b from-black/10 to-black/10 rounded-bl rounded-br">
-              <div className="text-white text-4xl mt-[-4rem] font-bold ">Rashi</div>
+              <div className="text-white text-4xl mt-[-4rem] font-bold ">
+                Rashi
+              </div>
             </div>
           </div>
+        </div>
+        <div className="self-center hidden lg:flex 2xl:hidden h-[58px] flex-col inset-x-0 bottom-0 justify-start items-start gap-2.5  ">
+          <BookAFreeDemoButton />
         </div>
       </div>
     </div>
