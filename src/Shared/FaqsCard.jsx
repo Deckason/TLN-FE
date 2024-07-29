a1534ac4fc35229cf2bfac6fc803c6e1991b5f67
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2024-07-30 00:12:52 +0530

    add pointUp svg and add to Faq section

--- src/Shared/FaqsCard.jsx
+++ src/Shared/FaqsCard.jsx
@@ -1,28 +1,35 @@
 "use client";
 import pointDown from "../Assets/Icons/pointDown.svg";
+import pointUp from "../Assets/Icons/pointUp.svg";
 import Image from "next/image";
 import { useState } from "react";
+
 const FaqsCard = ({ data, i }) => {
   const [showDes, setShowDes] = useState(false);
-  const [id, setId] = useState(i);
+
   return (
-    <div onClick={() => setShowDes(!showDes)} className="my-2 w-full ">
+    <div onClick={() => setShowDes(!showDes)} className="my-2 w-full">
       <div
         className={`${
-          showDes ? "h-full" : " min-h-[75px]"
-        }  p-4 xl:p-6 w-full shadow-md hover:shadow-xl transition  bg-white rounded-2xl transform duration-300 justify-center items-center flex-col flex`}
+          showDes ? "h-full" : "min-h-[75px]"
+        } p-4 xl:p-6 w-full shadow-md hover:shadow-xl transition bg-white rounded-2xl transform duration-300 justify-center items-center flex-col flex`}
       >
-        <div className="grow shrink basis-0 transform duration-300 self-stretch w-full justify-start items-center inline-flex ">
-          <div className="grow shrink basis-0  text-black text-xl  max-md:text-base max-sm:text-sm transform duration-300 font-medium w-full leading-7">
+        <div className="grow shrink basis-0 transform duration-300 self-stretch w-full justify-start items-center inline-flex">
+          <div className="grow shrink basis-0 text-black text-xl max-md:text-base max-sm:text-sm transform duration-300 font-medium w-full leading-7">
             {data?.question}
           </div>
-          <div className="max-md:w-4  w-[18px]  relative">
-            <Image alt="pointDown" src={pointDown} width={18} height={11} />
+          <div className="max-md:w-4 w-[18px] relative">
+            <Image
+              alt={showDes ? "pointUp" : "pointDown"}
+              src={showDes ? pointUp : pointDown}
+              width={18}
+              height={11}
+            />
           </div>
         </div>
         <div
           className={`${
-            showDes ? " opacity-100 mt-3 " : "hidden opacity-0"
+            showDes ? "opacity-100 mt-3" : "hidden opacity-0"
           } transform w-full max-md:text-sm duration-300 text-justify lg:ps-4 lg:me-8`}
         >
           {data?.answer}
