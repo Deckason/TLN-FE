87a59c701a28aba0d5b69b4b72302b8496e374ca
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2024-07-29 23:58:11 +0530

    add the infinite scroll in the Section2

--- src/app/(Home)/Navigation/ContentCreators/components/Section2.jsx
+++ src/app/(Home)/Navigation/ContentCreators/components/Section2.jsx
@@ -4,9 +4,18 @@ const Section2 = () => {
   return (
     <>
       <div>
-        <div className="w-full bg-primary-color max-sm:h-[200px] h-[380px] relative">
-          <div className="px-4 h-full flex items-center text-center text-white text-5xl max-sm:text-2xl max-lg:text-4xl font-bold justify-center">
-            Spread the joy of learning with us!
+        <div className="overflow-hidden  w-full bg-primary-color max-sm:h-[200px] h-[380px] relative flex justify-center items-center">
+
+          <div className="bg-white text-primary-color w-full  flex items-center justify-center text-center  ">
+
+            <div className="w-[934px] overflow-hidden h-24 flex items-center justify-center">
+
+              <p className=" animate-marquee  md:text-5xl whitespace-nowrap h-auto  text-5xl max-sm:text-3xl max-lg:text-5xl font-bold  ">
+
+                Spread the joy of learning with us! Spread the joy of learning with us! Spread the joy of learning with us! Spread the joy of learning with us!
+              </p>
+            </div>
+
           </div>
         </div>
       </div>
