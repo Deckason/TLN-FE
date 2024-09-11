8c6fe5b81afe1fb773db186d604d6c8c570b5289
Kaushik <kaushikjoshi827@gmail.com>
2024-09-11 22:18:00 +0530

    Fixed footer icons and improved the educational partners component slider

--- src/Components/HomeComp/EducationPaterners.jsx
+++ src/Components/HomeComp/EducationPaterners.jsx
@@ -5,91 +5,35 @@ import logo1 from "../../Assets/Homepage/Educationalbar/image_1.png";
 import logo2 from "../../Assets/Homepage/Educationalbar/image_2.png";
 import logo3 from "../../Assets/Homepage/Educationalbar/image_3.png";
 import logo4 from "../../Assets/Homepage/Educationalbar/image_4.png";
-import {Swiper, SwiperSlide} from 'swiper/react';
-import "swiper/css";
-import "swiper/css/pagination";
-import { Navigation } from "swiper/modules";
-import { useState } from "react";
-const EducationPartners = () => {
-  const [currentPage, setCurrentPage] = useState(0);
-  const navigationPrevRef = React.useRef(currentPage);
-  const navigationNextRef = React.useRef(currentPage);
-  const slides = [
-    {
-      src:logo1
-    },
-    {
-      src:logo2
-    },
-    {
-      src:logo3
-    },
-    {
-      src:logo4
-    },
-  ]
-  return (
-    <div>
-      <div className="my-[20px] lg:my-[50px] 2xl:my-[112px] max-w-[1681px]  mx-auto flex justify-center items-center ">
-        <section>
-          <div>
-            <h1 className="text-stone-900 text-[24px]/[29px] sm:text-[36px]/[45px] lg:text-[40px]/[50px] xl:text-[50px]/[60px] 2xl:text-[60px]/[72px] font-bold text-center mb-[20.9px]">
-              Our Educational Partners
-            </h1>
-          </div>
-          {/* <div className="flex  flex-wrap justify-center items-center w-full gap-[30px]">
-            <Image className="lg:w-full lg:flex " alt="logo" src={logo1} />
-          </div> */}
-          <div>
-            <Swiper
-            spaceBetween={20}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Navigation]}
-            navigation={{
-              prevEl: navigationPrevRef.current,
-              nextEl: navigationNextRef.current,
-            }}
-            onBeforeInit={(swiper) => {
-              swiper.params.navigation.prevEl = navigationPrevRef.current;
-              swiper.params.navigation.nextEl = navigationNextRef.current;
-            }}
-            onSlideChange={(swiper) => {
-              setCurrentPage(swiper.activeIndex);
-            }}
-            // slidesPerView={4}
-            breakpoints={{
-              1680: {
-                slidesPerView: 4,
-              },
-              1280: {
-                slidesPerView: 3,
-              },
-              1024: {
-                slidesPerView: 2,
-              },
-              768: {
-                slidesPerView: 2,
-              },
-              368: {
-                slidesPerView: 1,
-              },
-            }}
-            // modules={[Pagination]}
-            className="mySwiper mx-auto flex justify-center  items-center  lg:w-[1000px] w-[350px] xl:w-[1270px] gap-[10px]"
-          >
-            {
-              slides.map((item,i)=>(
-            <SwiperSlide key={i}>
-               <Image className="lg:w-full lg:flex h-[80px] lg:h-[90px] xl:h-[110px] object-contain " alt="logo" src={item.src} />
-            </SwiperSlide>
 
-              ))
-            }
-          </Swiper>
+const EducationPartners = () => {
+  // Array of logos
+  const logos = [logo1, logo2, logo3, logo4];
+
+  return (
+    <div className="w-full max-w-[1270px] 2xl:max-w-[1681px] mx-auto flex justify-center items-center mb-10">
+      <div>
+        <h1 className="text-stone-900 text-[24px]/[29px] sm:text-[36px]/[45px] lg:text-[40px]/[50px] xl:text-[50px]/[60px] 2xl:text-[60px]/[72px] font-bold text-center mb-[20.9px]">
+          Our Educational Partners
+        </h1>
+        <div className="flex flex-wrap justify-center items-center lg:w-[1000px] w-[350px] xl:w-[1270px] gap-[30px]">
+          <div className="slider">
+            <div className="slide-track">
+              {/* Use array to render the logos dynamically */}
+              {logos.concat(logos).map((logo, index) => (
+                <div className="slide w-[100px] h-[60px] flex items-center justify-center" key={index}>
+                  <Image 
+                    alt={`logo-${index}`} 
+                    src={logo} 
+                    className="object-contain" // Ensures the image scales correctly
+                    width={100} // Adjust the width of each image
+                    height={60} // Adjust the height of each image
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </section>
+        </div>
       </div>
     </div>
   );
