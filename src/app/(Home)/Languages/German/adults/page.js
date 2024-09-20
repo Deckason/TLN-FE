b5326a918bbf0d571002fe4900392dc111e78bfb
hemvish11 <hemvish11@gmail.com>
2024-09-20 22:55:31 +0530

    Adults and StudyAbroad FAQs are now integrated for all pages and rich text bug also solved

--- src/app/(Home)/Languages/German/adults/page.js
+++ src/app/(Home)/Languages/German/adults/page.js
@@ -1,11 +1,39 @@
-import {data} from "../../../../../Utilities/data/german/AdultData"
+"use client";
+import { useEffect, useState } from "react";
+import { useGetFilteredFAQsQuery } from "../../../../../store/apiSlice";
+
+import { data } from "../../../../../Utilities/data/german/AdultData";
 import React from "react";
 import SharedAdultsPage from "../../../../../Shared/SharedAdultsPage";
 
 const page = () => {
+  const { data: faqData } = useGetFilteredFAQsQuery({
+    language: "German",
+    context: "Adult",
+    category: "General",
+  });
+  const { data: everyData } = useGetFilteredFAQsQuery({
+    language: "German",
+    context: "Adult",
+    category: "Everything you need to know",
+  });
+
+  const [content, setContent] = useState(data);
+  useEffect(() => {
+    if (!faqData) return;
+    if (!everyData) return;
+    setContent((prev) => ({
+      ...prev,
+      Faqs: faqData,
+      EveryThingYouNeedToKnowAbout: {
+        ...prev.EveryThingYouNeedToKnowAbout,
+        CardData: everyData,
+      },
+    }));
+  }, [faqData, everyData]);
   return (
     <div>
-     <SharedAdultsPage Data={data} language={"German"} context ={"Adult"}/>
+      <SharedAdultsPage Data={content} language={"German"} context={"Adult"} />
     </div>
   );
 };
--- src/app/(Home)/Languages/Japanese/adults/page.js
+++ src/app/(Home)/Languages/Japanese/adults/page.js
@@ -1,12 +1,43 @@
-import {data} from "../../../../../Utilities/data/japanese/AdultData"
+"use client";
+import { useEffect, useState } from "react";
+import { useGetFilteredFAQsQuery } from "../../../../../store/apiSlice";
+
+import { data } from "../../../../../Utilities/data/japanese/AdultData";
 import React from "react";
 import SharedAdultsPage from "../../../../../Shared/SharedAdultsPage";
 
 const page = () => {
-  
+  const { data: faqData } = useGetFilteredFAQsQuery({
+    language: "Japanese",
+    context: "Adult",
+    category: "General",
+  });
+  const { data: everyData } = useGetFilteredFAQsQuery({
+    language: "Japanese",
+    context: "Adult",
+    category: "Everything you need to know",
+  });
+
+  const [content, setContent] = useState(data);
+  useEffect(() => {
+    if (!faqData) return;
+    if (!everyData) return;
+    setContent((prev) => ({
+      ...prev,
+      Faqs: faqData,
+      EveryThingYouNeedToKnowAbout: {
+        ...prev.EveryThingYouNeedToKnowAbout,
+        CardData: everyData,
+      },
+    }));
+  }, [faqData, everyData]);
   return (
     <div>
-     <SharedAdultsPage Data={data} language={"Japanese"} context ={"Adult"}/>
+      <SharedAdultsPage
+        Data={content}
+        language={"Japanese"}
+        context={"Adult"}
+      />
     </div>
   );
 };
