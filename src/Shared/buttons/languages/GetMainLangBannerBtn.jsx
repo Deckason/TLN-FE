3d56d4338bcd8659c8a2f1823cd6d6e6b98ae7d2
Kaushik <kaushikjoshi827@gmail.com>
2024-09-23 22:51:04 +0530

    Done some changes in content of different languages

--- src/Shared/buttons/languages/GetMainLangBannerBtn.jsx
+++ src/Shared/buttons/languages/GetMainLangBannerBtn.jsx
@@ -12,7 +12,7 @@ const GetMainLangBannerBtn = () => {
        focus:border-teal-600 border  hover:text-teal-600 hover:border-teal-600
         bg-teal-600 rounded-lg z-30 text-center"
       >
-        Get started
+        Book a Free Demo
       </button>
     </div>
   );
