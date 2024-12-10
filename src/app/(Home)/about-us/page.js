b1986aae4e0a1d141d8bffd7519022fdf8d79446
archita-2810 <architasugandh@gmail.com>
2024-12-10 19:25:26 +0530

    minor changes

--- src/app/(Home)/about-us/page.js
+++ src/app/(Home)/about-us/page.js
@@ -21,11 +21,13 @@ const Aboutus = () => {
 
     useEffect(() => {
         // Check if screen width is mobile size and set isMobile state
-        const handleResize = () => setIsMobile(window.innerWidth <= 768);
-        handleResize(); // Initial check
+        if(typeof window!=='undefined'){
+            const handleResize = () => setIsMobile(window.innerWidth <= 768);
+            handleResize(); // Initial check
 
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
+            window.addEventListener("resize", handleResize);
+            return () => window.removeEventListener("resize", handleResize);
+        }
     }, []);
 
     const [isExpanded, setIsExpanded] = useState(false);
